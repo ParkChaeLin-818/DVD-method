53df0f3fe250801fa5655531d68236e5b020e7ac
ParkChaeLin-818 <qkrcofls6863@gmail.com>
2020-06-11 18:29:47 +0900

    전체적이코드수정및주석문 작성했습니다.

--- DVDSaleCalculate.java
+++ DVDSaleCalculate.java
@@ -1,11 +1,19 @@
 /**
- * 여기에 DVDSaleCalculate 클래스 설명을 작성하십시오.
+ * 렌탈하는 DVD의 할인여부에 따라 요금을 계산해주는 메소드이다.
  * 
  * @author (박채린 , 이와사키메구미 , 마츠바라케이토) 
  * @version (2020.06.11)
  */
 public class DVDSaleCalculate
 {
+    /**
+     * @param
+     * Date = 구작(대여시작일로부터 6개월이상 경과된 작품) 인지 아닌지 판단한 값
+     * (True = 구작일 경우 / False = 구작이 아닐경우)
+     * age = 나이값
+     * charge = 요금
+     * @return = 할인이 적용된 후의 요금
+     */
     boolean Date = true;
     int age = 0;
     int charge = 5000;
@@ -14,19 +22,17 @@ public class DVDSaleCalculate
         if (Date == true){
              charge = charge / 2;
         }
-        else if (Date == false){
+        else{
             if(age < 19){
                 charge = charge / 10 * 9;
-           }
-           else if(age > 64){
+            }
+            else if(age > 64){
                charge = charge / 5 * 4;
-           }
-           else{
+            }
+            else{
                charge = charge;
             }
         }
-        
-        
         return charge;
     }
 }
